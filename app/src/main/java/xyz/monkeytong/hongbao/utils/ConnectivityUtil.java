107a5fb948010fb0751daae940031a0283865b7e
golden <huaguoting@gmail.com>
2016-10-25 16:48:12 +0800

    reformat code.

--- app/src/main/java/xyz/monkeytong/hongbao/utils/ConnectivityUtil.java
+++ app/src/main/java/xyz/monkeytong/hongbao/utils/ConnectivityUtil.java
@@ -8,12 +8,13 @@ import android.net.NetworkInfo;
  * Created by Zhongyi on 1/29/16.
  */
 public class ConnectivityUtil {
-    public static boolean isWifi(Context context) {
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+  public static boolean isWifi(Context context) {
+    ConnectivityManager cm =
+        (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting()
-                && activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
-    }
+    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+    return activeNetwork != null
+        && activeNetwork.isConnectedOrConnecting()
+        && activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+  }
 }
